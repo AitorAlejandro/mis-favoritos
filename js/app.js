015fa29cf7c5feccceed156b901da8bb346af22b
Aitor Alejandro Herrera <aitor.alejandro.herrera@gmail.com>
2017-03-17 20:40:06 +0100

    Send with us

--- js/app.js
+++ js/app.js
@@ -199,7 +199,7 @@ var objHerramientas =  [
     {
         url: "https://www.sendwithus.com/",
         linkText: "Send with us",
-        text: "Te permite realizar envios de email para comprobar tus propios diseños en los distintos clientes web. Lo permite hacer de forma más rápida que <a href=\"https://mailchimp.com/" target=\"_blank\">mailchimp</a>, simplemente crea tu diseño y haz el envio."
+        text: "Te permite realizar envios de email para comprobar tus propios diseños en los distintos clientes web. Lo permite hacer de forma más rápida que <a href=\"https://mailchimp.com/\" target=\"_blank\">mailchimp</a>, simplemente crea tu diseño y haz el envio."
     }
 ];
 
